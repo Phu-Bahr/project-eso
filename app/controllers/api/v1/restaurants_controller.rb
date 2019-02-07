56a4345673839c8d07f39b33c06ebd9edb7aabf1
Tony Phu <tonyphu@Tonys-MBP.hsd1.ma.comcast.net>
2019-02-06 23:53:46 -0500

    testing parser

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -2,25 +2,27 @@ class Api::V1::RestaurantsController < ApplicationController
 
   def search
     restaurant_parser = RestaurantParser.new
-    # grab ALL of the params from the form
-    choice = params[:choice]
+    categories = params[:categories]
     location = params[:location]
     price = params[:price]
-    restaurant_parser.search(choice, location, price)
+    # categories = "italian"
+    # location = "boston, ma"
+    # price = 2
+    restaurant_parser.search(categories, location, price)
 
-    session[:yelp_data] = restaurant_parser.data
+    # session[:yelp_data] = restaurant_parser.data
 
     render json: { data: restaurant_parser.data }
   end
 
-  def index
-    yelp_data = session[:yelp_data]
-    session[:yelp_data] = nil
-
-    # maybe clean up the yelp_data to only have the most necessary info
-    # then pass it back to react
-
-    render json: {data: yelp_data}
-  end
+  # def index
+  #   yelp_data = session[:yelp_data]
+  #   session[:yelp_data] = nil
+  #
+  #   # maybe clean up the yelp_data to only have the most necessary info
+  #   # then pass it back to react
+  #
+  #   render json: {data: yelp_data}
+  # end
 
 end
