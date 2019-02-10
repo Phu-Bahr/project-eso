f357ebc3e65e33add0cbd3a5fd4b15684cb2f143
Tony Phu <tonyphu@Tonys-MBP.hsd1.ma.comcast.net>
2019-02-10 08:39:38 -0500

    added logic to clicking like and adding state, but props not sending to parent do to async issue? error fetch addrestaurant not defined

--- app/javascript/containers/RestaurantContainer.js
+++ app/javascript/containers/RestaurantContainer.js
@@ -6,15 +6,34 @@ class RestaurantContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      restaurants: []
+      liked: [],
+      dislike: []
     };
+
+    this.addLikedRestaurant = this.addLikedRestaurant.bind(this);
+    this.setCurrentRestaurant = this.setCurrentRestaurant.bind(this);
+  }
+
+  setCurrentRestaurant(setID) {
+    this.setState({ liked: setID });
+  }
+
+  addLikedRestaurant(event) {
+    event.preventDefault();
+    let likePayload = this.state.liked;
+    this.props.addLiked(likePayload);
   }
 
   render() {
+    console.log("Restaurants Container state =>", this.state);
     let yelpList = [];
     if (this.props.yelpdata) {
       let yelpData = this.props.yelpdata;
       yelpList = yelpData.map(restaurant => {
+        let handleLikeClick = () => {
+          this.setCurrentRestaurant(restaurant);
+        };
+
         return (
           <RestaurantTile
             key={restaurant.id}
@@ -27,6 +46,8 @@ class RestaurantContainer extends Component {
             imageUrl={restaurant.image_url}
             coordinates={restaurant.coordinates}
             name={restaurant.name}
+            like={handleLikeClick}
+            confirm={addLikedRestaurant}
           />
         );
       });
@@ -36,8 +57,8 @@ class RestaurantContainer extends Component {
         <div className="restaurant-show-row">
           <div>{yelpList}</div>
         </div>
-        <div>
-          <h1 className="row" align="center">
+        <div className="row">
+          <h1>
             <Link to="/restaurants" className="text-center">
               Your Restaurants
             </Link>
