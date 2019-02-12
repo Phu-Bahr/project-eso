7f1e104a15bf4c6b25be7190875522388f5fc29e
Tony Phu <tonyphu@Tonys-MacBook-Pro.local>
2019-02-12 16:21:52 -0500

    added columns to restaurants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_07_051431) do
+ActiveRecord::Schema.define(version: 2019_02_12_211737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 2019_02_07_051431) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "alias"
-    t.string "coordinates"
     t.string "price"
     t.integer "rating"
     t.string "name", null: false
@@ -54,6 +53,10 @@ ActiveRecord::Schema.define(version: 2019_02_07_051431) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "yelpcategory", array: true
+    t.integer "review_count"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
