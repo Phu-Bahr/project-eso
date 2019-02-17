7c619b74fb6e4fd384e21fad4b843d592f8ec396
Tony Phu <tonyphu@Tonys-MBP.hsd1.ma.comcast.net>
2019-02-17 12:42:28 -0500

    fixed categories

--- app/javascript/tiles/CategoryTile.js
+++ app/javascript/tiles/CategoryTile.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const CategoryTile = props => {
-  return <div />;
+  return (
+    <div
+      className={"small-3 columns show-tile fade" + " " + props.highlighted}
+      onClick={props.handleClick}
+    >
+      <div className="center-rest-tile">
+        <div className="rest-tile-text">{props.name}</div>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryTile;
--- app/javascript/tiles/RestaurantIndexTile.js
+++ app/javascript/tiles/RestaurantIndexTile.js
@@ -2,15 +2,7 @@ import React from "react";
 import { Link } from "react-router";
 
 const RestaurantIndexTile = props => {
-  return (
-    <div>
-      <li>
-        <Link to={props.url} target="blank" className="text-center">
-          {props.name}
-        </Link>
-      </li>
-    </div>
-  );
+  return <div />;
 };
 
 export default RestaurantIndexTile;
