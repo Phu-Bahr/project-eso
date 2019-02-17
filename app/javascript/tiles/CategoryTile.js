aede9c3437d3a191c7c5650d4f5dd79220fc9b3b
Tony Phu <tonyphu@Tonys-MBP.hsd1.ma.comcast.net>
2019-02-17 12:36:38 -0500

    reviewing white space in restaurants page

--- app/javascript/tiles/CategoryTile.js
+++ app/javascript/tiles/CategoryTile.js
@@ -1,16 +1,7 @@
 import React from "react";
 
 const CategoryTile = props => {
-  return (
-    <div
-      className={"small-3 columns show-tile fade" + " " + props.highlighted}
-      onClick={props.handleClick}
-    >
-      <div className="center-rest-tile">
-        <div className="rest-tile-text">{props.name}</div>
-      </div>
-    </div>
-  );
+  return <div />;
 };
 
 export default CategoryTile;
