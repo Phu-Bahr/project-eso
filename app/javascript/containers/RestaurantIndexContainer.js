09c6a8ceba54dfca2bc752df78fac9c4f05e9919
Tony Phu <tphu118@gmail.com>
2019-12-19 10:30:21 -0500

    cleaned up code, and added sort to columns for tables

--- app/javascript/containers/RestaurantIndexContainer.js
+++ app/javascript/containers/RestaurantIndexContainer.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { useTable } from "react-table";
-import makeData from "./makeData";
-import MakeData1 from "./makeData1";
+import { useTable, useSortBy } from "react-table";
 
 const Styles = styled.div`
   padding: 1rem;
@@ -41,42 +39,68 @@ function Table({ columns, data }) {
     headerGroups,
     rows,
     prepareRow
-  } = useTable({
-    columns,
-    data
-  });
+  } = useTable(
+    {
+      columns,
+      data
+    },
+    useSortBy
+  );
 
+  const firstPageRows = rows.slice(0, 20);
   // Render the UI for your table
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-              })}
+    <div>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                // Add the sorting props to control sorting. For this example
+                // we can add them into the header props
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  {/* Add a sort direction indicator */}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? " 🔽"
+                        : " 🔼"
+                      : ""}
+                  </span>
+                </th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {firstPageRows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <br />
+      <div>
+        Showing the first 20 results of {rows.length} rows. Click on column
+        names to sort.
+      </div>
+    </div>
   );
 }
 
 // how to fetch my data with hooks.
 const RestaurantIndexContainer = () => {
   const [data1, setData] = useState([]);
+  const [user, setUser] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -95,14 +119,17 @@ const RestaurantIndexContainer = () => {
       })
       .then(response => response.json())
       .then(body => {
+        console.log("body", body.restaurants[0].UserName);
+
         setData(body.restaurants[0].UserRestaurants);
+        setUser(body.restaurants[0].UserName);
       });
   };
 
   const columns = React.useMemo(
     () => [
       {
-        Header: "Username Restaurants",
+        Header: "Your Restaurants",
         columns: [
           {
             Header: "Name",
